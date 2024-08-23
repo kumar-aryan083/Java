76b67e5d5b2826b4898eccdfacb32fe2d734782c
Aryan <aryans6204@gmail.com>
2024-08-23 12:53:13 +0530

    completed recursion part 1

--- Recursion.java
+++ Recursion.java
@@ -1,15 +1,37 @@
 public class Recursion{
     public static void main(String[] args) {
-        // System.out.println("Hello java");
-        System.out.println(factorial(5));
-        printNumbers(8);
-        System.out.println();
-        printNumbers1(8);
-        System.out.println();
-        System.out.println(printSum(5));
-        System.out.println(fib(6));
-    }
+        //---------------------------------------------------------------------------
+        // Recursion part 1
+        //---------------------------------------------------------------------------
 
+        // RecursionPart1 obj = new RecursionPart1();
+        // System.out.println(obj.factorial(5));
+        // obj.printNumbers(8);
+        // System.out.println();
+        // obj.printNumbers1(8);
+        // System.out.println();
+        // System.out.println(obj.printSum(5));
+        // System.out.println(obj.fib(6));
+        // int[] arr = {1,2,3,4,5, 2, 1,5,4};
+        // if(obj.checkSorted(arr, 0)){
+        //     System.out.println("Sorted");
+        // }else{
+        //     System.out.println("Not sorted");
+        // }
+        // System.out.println(obj.firstOccurence(arr, 0, 5));
+        // System.out.println(obj.lastOccurence(arr, arr.length-1, 5));
+        // System.out.println(obj.pow(2, 10));
+        // System.out.println(obj.optimizedPow(2, 5));
+
+        //---------------------------------------------------------------------------
+        // Recursion Part 2
+        //---------------------------------------------------------------------------
+
+        System.out.println("Hello Recursion part 2");
+    }
+}
+
+class RecursionPart1{
     // factorial of a number
     static int factorial(int n){
         if(n == 0) return 1;
@@ -43,5 +65,40 @@ public class Recursion{
         if(n == 1 || n == 0) return n;
         int fibn = fib(n-1)+fib(n-2);
         return fibn;
+    }
+    //check if array is sorted or not
+    static boolean checkSorted(int arr[], int i){
+        if(i == arr.length-1) return true;
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return checkSorted(arr, i+1);
+    }
+    //find first occurence of an element
+    static int firstOccurence(int[] arr, int i, int target){
+        if(i == arr.length) return -1;
+        if(arr[i] == target) return i;
+        return firstOccurence(arr, i+1, target);
+    }
+    //find last occurence of an element
+    static int lastOccurence(int[] arr, int i, int target){
+        if(i < 0) return -1;
+        if(arr[i] == target) return i;
+        return firstOccurence(arr, i-1, target);
+    }
+    //find x^n
+    public static int pow(int x, int n){
+        if(n == 0) return 1;
+        return x * pow(x, n-1);
+    }
+    //find x^n in O(logN)
+    public static int optimizedPow(int x, int n){
+        if(n == 0) return 1;
+        int powHalf = optimizedPow(x, n/2);
+        int powHalfSq = powHalf*powHalf;
+        if(n%2!=0){
+            powHalfSq = x * powHalfSq;
+        }
+        return powHalfSq;
     }
 }
